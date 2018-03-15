450d162c7b96764c0b8389b588073991de24c3dc
Marc Kelly <marc.kelly@iceotope.com>
2018-03-15 11:14:38 +0000

    Altering the gpio setup to use arrays and have it make symbolic links.

--- meta-exanest/meta-track2/recipes-track2/fbutils/files/setup-gpio.sh
+++ meta-exanest/meta-track2/recipes-track2/fbutils/files/setup-gpio.sh
@@ -37,88 +37,98 @@
 . /usr/local/fbpackages/utils/ast-functions
 . /usr/local/fbpackages/utils/gpio_names.sh
 
+## Create the structure of the board with symlinks
+rm -rf /tmp/mezzanine
+mkdir -p /tmp/mezzanine
+mkdir -p /tmp/mezzanine/gpio
+for i in `seq 0 15`
+do
+    mkdir -p /tmp/mezzanine/site_${i}
+    mkdir -p /tmp/mezzanine/site_${i}/gpio
+    mkdir -p /tmp/mezzanine/db_${i}
+done
+
 ## Do all the exporting
 
 # I2C
 gpio_export_out ${SITE_I2C_RST}
 gpio_export_out ${DB_I2C_RST}
 
+ln -s /sys/class/gpio/gpio${SITE_I2C_RST} /tmp/mezzanine/gpio/SITE_I2C_RST
+ln -s /sys/class/gpio/gpio${DB_I2C_RST} /tmp/mezzanine/gpio/DB_I2C_RST
+
 # Active low reset, so put them in normal mode
 gpio_set ${SITE_I2C_RST} 1
 gpio_set ${DB_I2C_RST} 1
 
-# Slot ID
-gpio_export ${SLOT_ID_0}
-gpio_export ${SLOT_ID_1}
-gpio_export ${SLOT_ID_2}
-gpio_export ${SLOT_ID_3}
-
 # EEProm WP
 gpio_export_out ${EEPROM_WP}
+ln -s /sys/class/gpio/gpio${EEPROM_WP} /tmp/mezzanine/gpio/EEPROM_WP
 gpio_set ${EEPROM_WP} 0
 
 # Power on (48V)
 gpio_export_out ${PS_ON}
+ln -s /sys/class/gpio/gpio${PS_ON} /tmp/mezzanine/gpio/PS_ON
 gpio_set ${PS_ON} 0
-
-
-# IRQ's from the IO Expanders
-gpio_export ${CTL_REG_INT_0}
-gpio_export ${CTL_REG_INT_1}
-gpio_export ${CTL_REG_INT_2}
-gpio_export ${CTL_REG_INT_3}
-gpio_export ${CTL_REG_INT_4}
-gpio_export ${CTL_REG_INT_5}
-gpio_export ${CTL_REG_INT_6}
-gpio_export ${CTL_REG_INT_7}
 
 # 2v5 enable, turns on the retimers
 gpio_export_out ${REG_2V5_ENABLE}
+ln -s /sys/class/gpio/gpio${REG_2V5_ENABLE} /tmp/mezzanine/gpio/REG_2V5_ENABLE
 gpio_set ${REG_2V5_ENABLE} 1
 
 # Retimer resets
 gpio_export_out ${RETIMER_RESET}
+ln -s /sys/class/gpio/gpio${RETIMER_RESET} /tmp/mezzanine/gpio/RETIMER_RESET
 gpio_set ${RETIMER_RESET} 0
 
 # Internal Serial loopback
 gpio_export_out ${SERIAL_LOOPBACK}
+ln -s /sys/class/gpio/gpio${SERIAL_LOOPBACK} /tmp/mezzanine/gpio/SERIAL_LOOPBACK
 gpio_set ${SERIAL_LOOPBACK} 0
 
+
+# Slot ID
+index=0
+mkdir -p /tmp/mezzanine/gpio/SLOT_ID
+for i in "${SLOT_ID[@]}"
+do
+   gpio_export ${i}
+   ln -s /sys/class/gpio/gpio${i} /tmp/mezzanine/gpio/SLOT_ID/${index}
+   index=$((index+1))
+done
+
+# IRQ's from the IO Expanders
+index=0
+mkdir -p /tmp/mezzanine/gpio/CTL_REG_INT
+for i in "${CTL_REG_INT[@]}"
+do
+   gpio_export ${i}
+   ln -s /sys/class/gpio/gpio${i} /tmp/mezzanine/gpio/CTL_REG_INT/${index}
+   index=$((index+1))
+done
+
 # I2C Expander on base board
-gpio_export_out ${RST_CTLREG_0}
-gpio_export_out ${RST_CTLREG_1}
-gpio_export_out ${RST_CTLREG_2}
-gpio_export_out ${RST_CTLREG_3}
-gpio_export_out ${RST_CTLREG_4}
-gpio_export_out ${RST_CTLREG_5}
-gpio_export_out ${RST_CTLREG_6}
-gpio_export_out ${RST_CTLREG_7}
-gpio_export_out ${RST_CTLREG_8}
-gpio_export_out ${RST_CTLREG_9}
-gpio_export_out ${RST_CTLREG_10}
-gpio_export_out ${RST_CTLREG_11}
-gpio_export_out ${RST_CTLREG_12}
-gpio_export_out ${RST_CTLREG_13}
-gpio_export_out ${RST_CTLREG_14}
-gpio_export_out ${RST_CTLREG_15}
+# IRQ's from the IO Expanders
+index=0
+mkdir -p /tmp/mezzanine/gpio/RST_CTLREG
+for i in "${RST_CTLREG[@]}"
+do
+   gpio_export_out ${i}
+   gpio_set ${i} 1
+   ln -s /sys/class/gpio/gpio${i} /tmp/mezzanine/gpio/RST_CTLREG/${index}
+   index=$((index+1))
+done
 
-gpio_set ${RST_CTLREG_0} 1
-gpio_set ${RST_CTLREG_1} 1
-gpio_set ${RST_CTLREG_2} 1
-gpio_set ${RST_CTLREG_3} 1
-gpio_set ${RST_CTLREG_4} 1
-gpio_set ${RST_CTLREG_5} 1
-gpio_set ${RST_CTLREG_6} 1
-gpio_set ${RST_CTLREG_7} 1
-gpio_set ${RST_CTLREG_8} 1
-gpio_set ${RST_CTLREG_9} 1
-gpio_set ${RST_CTLREG_10} 1
-gpio_set ${RST_CTLREG_11} 1
-gpio_set ${RST_CTLREG_12} 1
-gpio_set ${RST_CTLREG_13} 1
-gpio_set ${RST_CTLREG_14} 1
-gpio_set ${RST_CTLREG_15} 1
+## One wire bus here, we set links for the sensors at the sites to the directories
 
+for i in `seq 0 15`
+do
+    master="/sys/devices/w1_bus_master$((i+1))"
+    slave=`cat ${master}/w1_master_slaves`
+    ln -s ${master}/${slave}/w1_slave /tmp/mezzanine/site_${i}/temperature
+done
+
+## Now we can search the i2c sub busses an bind all the ioexpanders.
 
 # Yosemite OOM remediation
 #   enable kernel panic (force reboot)
