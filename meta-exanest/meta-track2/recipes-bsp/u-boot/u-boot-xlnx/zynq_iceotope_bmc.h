29874dc2d0d62788ce8e22432d6e003b17c93483
Marc Kelly <marc.kelly@iceotope.com>
2018-03-16 17:11:55 +0000

    Stuff to update the uboot side of things to load fit images.
    Bumped the uboot we use to xilinx-v2016.3 version, as it can use fit
    images to flash fpga as well.
    Altered the uboot config files and how the patches are done.
    Changes from 2016.1->.3 mean altering the config and intilization files.
    Extra recipe steps to deal with it.

---
+++ meta-exanest/meta-track2/recipes-bsp/u-boot/u-boot-xlnx/zynq_iceotope_bmc.h
@@ -0,0 +1,90 @@
+/*
+ * (C) Copyright 2013 Xilinx, Inc.
+ *
+ * Configuration for Micro Zynq Evaluation and Development Board - MicroZedBoard
+ * See zynq-common.h for Zynq common configs
+ *
+ * SPDX-License-Identifier:  GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQ_ICEOTOPE_BMC_H
+#define __CONFIG_ZYNQ_ICEOTOPE_BMC_H
+
+
+#define CONFIG_SYS_NO_FLASH
+
+#define CONFIG_ZYNQ_SDHCI0
+#define CONFIG_ZYNQ_USB
+
+/*
+ * A custom env for our usage, remove lots of crap
+ */
+
+#define CONFIG_EXTRA_ENV_SETTINGS	\
+	"ethaddr=00:0a:35:00:01:22\0"	\
+	"kernel_image=uImage\0"	\
+	"kernel_load_address=0x2080000\0" \
+	"ramdisk_image=uramdisk.image.gz\0"	\
+	"ramdisk_load_address=0x4000000\0"	\
+	"devicetree_image=devicetree.dtb\0"	\
+	"devicetree_load_address=0x2000000\0"	\
+	"bitstream_image=system.bit.bin\0"	\
+	"boot_image=BOOT.bin\0"	\
+	"fit_image=openbmc_fit.itb\0" \
+	"loadbit_addr=0x100000\0"	\
+	"loadbootenv_addr=0x2000000\0" \
+	"kernel_size=0x500000\0"	\
+	"devicetree_size=0x20000\0"	\
+	"ramdisk_size=0x5E0000\0"	\
+	"boot_size=0xF00000\0"	\
+	"fdt_high=0x40000000\0"	\
+	"initrd_high=0x40000000\0"	\
+	"bootenv=uEnv.txt\0" \
+	"loadbootenv=load mmc 0 ${loadbootenv_addr} ${bootenv}\0" \
+	"importbootenv=echo Importing environment from SD ...; " \
+		"env import -t ${loadbootenv_addr} $filesize\0" \
+	"sd_uEnvtxt_existence_test=test -e mmc 0 /uEnv.txt\0" \
+	"preboot=if test $modeboot = sdboot && env run sd_uEnvtxt_existence_test; " \
+			"then if env run loadbootenv; " \
+				"then env run importbootenv; " \
+			"fi; " \
+		"fi; \0" \
+	"mmc_loadbit=echo Loading bitstream from SD/MMC/eMMC to RAM.. && " \
+		"mmcinfo && " \
+		"load mmc 0 ${loadbit_addr} ${bitstream_image} && " \
+		"fpga load 0 ${loadbit_addr} ${filesize}\0" \
+	"uenvboot=" \
+		"if run loadbootenv; then " \
+			"echo Loaded environment from ${bootenv}; " \
+			"run importbootenv; " \
+		"fi; " \
+		"if test -n $uenvcmd; then " \
+			"echo Running uenvcmd ...; " \
+			"run uenvcmd; " \
+		"fi\0" \
+	"sdboot=if mmcinfo; then " \
+			"run uenvboot; " \
+			"echo Copying Linux from SD to RAM... && " \
+			"load mmc 0 ${kernel_load_address} ${kernel_image} && " \
+			"load mmc 0 ${devicetree_load_address} ${devicetree_image} && " \
+			"load mmc 0 ${ramdisk_load_address} ${ramdisk_image} && " \
+			"bootm ${kernel_load_address} ${ramdisk_load_address} ${devicetree_load_address}; " \
+		"fi\0" \
+	"jtagboot=echo TFTPing Linux to RAM... && " \
+		"tftpboot ${kernel_load_address} ${kernel_image} && " \
+		"tftpboot ${devicetree_load_address} ${devicetree_image} && " \
+		"tftpboot ${ramdisk_load_address} ${ramdisk_image} && " \
+		"bootm ${kernel_load_address} ${ramdisk_load_address} ${devicetree_load_address}\0" \
+	"fitboot=echo TFTPing Linux to RAM... && " \
+		"dhcp ${kernel_load_address} ${fit_image} && " \
+		"bootm \0" \
+
+
+#include <configs/zynq-common.h>
+
+/* Remove this, we want to use the IP given by the DHCP */
+#ifdef CONFIG_BOOTP_SERVERIP
+#undef CONFIG_BOOTP_SERVERIP
+#endif
+
+#endif /* __CONFIG_ZYNQ_ICEOTOPE_BMC_H */
--- meta-exanest/meta-track2/recipes-bsp/u-boot/u-boot-xlnx_2016.01.bbappend
+++ meta-exanest/meta-track2/recipes-bsp/u-boot/u-boot-xlnx_2016.01.bbappend
@@ -1,8 +1,12 @@
 # This is a uboot extra customisation for our verison,
 # we need the custom platform init
 
+# this matches u-boot-xlnx 'xilinx-v2016.3' release tag
+SRCREV = "95e11f6eb4bc07bbee12a4217c58146bddac61b2"
+PV = "v2016.03${XILINX_EXTENSION}+git${SRCPV}"
 
-DEPENDS += "platform-init"
+## Removed, we copy the files in directly after unpack/patch stage
+#DEPENDS += "platform-init"
 
 FILESEXTRAPATHS_prepend := "${THISDIR}/u-boot-xlnx:"
 
@@ -14,4 +18,17 @@ SRC_URI_append_track2 = " file://uEnv.txt"
 
 ## Adds our board config into the mix
 SRC_URI_append = " file://0001-Adding-in-the-iceotop-bmc-config.patch \
+    file://ps7_init_gpl.c \
+    file://ps7_init_gpl.h \
+    file://zynq_iceotope_bmc.h\
+    file://zynq_iceotope_bmc_defconfig\
     "
+do_configure_prepend () {
+  # Copy over our inits
+    install -d ${S}/board/xilinx/zynq/zynq_iceotope_bmc/
+    cp -f ${WORKDIR}/ps7_init_gpl.h ${S}/board/xilinx/zynq/zynq_iceotope_bmc/
+    cp -f ${WORKDIR}/ps7_init_gpl.c ${S}/board/xilinx/zynq/zynq_iceotope_bmc/
+    cp -f ${WORKDIR}/zynq_iceotope_bmc.h  ${S}/include/configs/zynq_iceotope_bmc.h
+    cp -f ${WORKDIR}/zynq_iceotope_bmc_defconfig  ${S}/configs/zynq_iceotope_bmc_defconfig
+    cp -f ${S}/arch/arm/dts/zynq-microzed.dts ${S}/arch/arm/dts/zynq-iceotope_bmc.dts
+}
